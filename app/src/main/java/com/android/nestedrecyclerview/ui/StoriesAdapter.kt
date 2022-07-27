59cadbc9d1d06b800ad31cf2d9ed2e1d64a33f20
alkomity <197023.it.eng@student.aden-univ.net>
2022-07-27 14:14:47 +0300

    fix problem doesn't show stories in main by make stories adapter with one view holder

--- app/src/main/java/com/android/nestedrecyclerview/ui/StoriesAdapter.kt
+++ app/src/main/java/com/android/nestedrecyclerview/ui/StoriesAdapter.kt
@@ -13,57 +13,20 @@ import com.android.nestedrecyclerview.databinding.StoryLayoutBinding
 import com.google.android.material.snackbar.Snackbar
 
 class StoriesAdapter(private val items : List<Story>) :
-    RecyclerView.Adapter<StoriesAdapter.BaseStoryViewHolder>()
+    RecyclerView.Adapter<StoriesAdapter.StoryViewHolder>()
 {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseStoryViewHolder {
-        return when(viewType){
-            VIEW_ADD_STORY -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.add_story,parent,false)
-                AddStoryViewHolder(view)
-            }
-            VIEW_STORY -> {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.item_stories,parent,false)
-                StoryViewHolder(view)
-            }
-            else ->  throw Exception("Unknown")
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoryViewHolder {
+             val view = LayoutInflater.from(parent.context).inflate(R.layout.story_layout,parent,false)
+             return StoryViewHolder(view)
     }
-
-    override fun onBindViewHolder(holder: BaseStoryViewHolder, position: Int) {
-        when(holder) {
-            is AddStoryViewHolder -> bindAddStory(holder,position)
-            is StoryViewHolder -> bindStory(holder,position)
-        }
-    }
-
-    private fun bindAddStory (holder: AddStoryViewHolder,position: Int){
-    }
-    private fun bindStory(holder : StoryViewHolder,position: Int){
-        val currentStory = items[position - 1]
+    override fun onBindViewHolder(holder: StoryViewHolder, position: Int) {
+        val currentStory = items[position]
         holder.binding.apply {
             imgStory.setImageResource(currentStory.imgUrl)
         }
     }
-
-    override fun getItemViewType(position: Int): Int {
-        return when(position) {
-            0 -> VIEW_ADD_STORY
-            else -> VIEW_STORY
-        }
-    }
-
-    override fun getItemCount(): Int = items.size + 1
-
-    abstract class BaseStoryViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem)
-    class AddStoryViewHolder(viewItem: View) : BaseStoryViewHolder(viewItem) {
-        val binding = AddStoryBinding.bind(viewItem)
-    }
-    class StoryViewHolder(viewItem: View) : BaseStoryViewHolder(viewItem) {
+    override fun getItemCount(): Int = items.size
+    class StoryViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
         val binding = StoryLayoutBinding.bind(viewItem)
-    }
-    companion object{
-        const val VIEW_ADD_STORY = 0
-        const val VIEW_STORY = 1
     }
 }
